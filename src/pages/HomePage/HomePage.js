3f5f006680f8c515a57124ec018f3828fb7cf79e
W.H.Wang <wh@bizlution.com>
2020-03-17 17:48:34 +0800

    finished homepage

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,20 +1,51 @@
 import React from 'react';
-import { Layout, Breadcrumb, Divider, Typography } from 'antd';
+import { withRouter } from 'react-router-dom';
+import { Layout, Typography, Button } from 'antd';
 import 'antd/dist/antd.css';
 
 import PageContent from '../../components/PageContent/PageContent';
 import Footer from '../../components/Footer/Footer';
 
-const HomePage = () => (
-  <Layout.Content style={{ padding: '20px' }}>
-    <Breadcrumb>
-      <Breadcrumb.Item>首頁</Breadcrumb.Item>
-    </Breadcrumb>
-    <Divider />
-    <Typography.Title level={2}>首頁</Typography.Title>
-    <PageContent>home page content here</PageContent>
-    <Footer />
-  </Layout.Content>
-);
+const HomePage = ({ history }) => {
+  const { Title } = Typography;
 
-export default HomePage;
+  const clickHandler = path => history.push(`/${path}`);
+
+  return (
+    <Layout.Content style={{ padding: '20px' }}>
+      <Title level={2}>首頁</Title>
+      <PageContent>
+        <Title level={3} style={{ textAlign: 'center' }} underline>
+          歡迎使用智慧宅 KIOSK 管理系統
+        </Title>
+        <Title level={4} style={{ textAlign: 'center' }}>
+          首頁輪播管理相關操作請前往
+          <Button
+            type="primary"
+            shape="round"
+            size="large"
+            style={{ marginLeft: '10px' }}
+            onClick={() => clickHandler('carousel')}
+          >
+            首頁輪播管理
+          </Button>
+        </Title>
+        <Title level={4} style={{ textAlign: 'center' }}>
+          公告管理相關操作請前往
+          <Button
+            type="primary"
+            shape="round"
+            size="large"
+            style={{ marginLeft: '10px' }}
+            onClick={() => clickHandler('announcement')}
+          >
+            公告管理
+          </Button>
+        </Title>
+      </PageContent>
+      <Footer />
+    </Layout.Content>
+  );
+};
+
+export default withRouter(HomePage);
